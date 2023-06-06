032f3ccea5344b0412701bd644666799e8645f68
Rodrigo rdr <rdr.alves@gmail.com>
2023-06-05 22:33:32 -0300

    update

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -79,10 +79,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 WSGI_APPLICATION = 'Shopping.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
 
 
 DATABASES = {
